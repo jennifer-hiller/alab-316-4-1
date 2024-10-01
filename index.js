56442ac7ae88ec5482329bea992efed2d686f39d
Jennifer Hiller <jennhi@gmail.com>
2024-10-01 10:24:14 -0700

    Made error message cumulative

--- index.js
+++ index.js
@@ -14,47 +14,47 @@ registrationForm.addEventListener("submit", (event) => {
   let errorMessage = "";
   const formData = new FormData(registrationForm);
   if (formData.get("username") === "") {
-    errorMessage = "Username cannot be empty";
+    errorMessage += "Username cannot be empty. ";
   } else if (formData.get("username").length < 4) {
-    errorMessage = "Username must be at least 4 characters long";
+    errorMessage += "Username must be at least 4 characters long. ";
     // value cannot contain spaces or whitespace
   } else if (formData.get("username").match(/\s/g)) {
-    errorMessage = "Username cannot contain whitespace";
+    errorMessage += "Username cannot contain whitespace. ";
     // value needs at least 2 unique characters
   } else if (new Set(formData.get("username")).size < 2) {
-    errorMessage = "Username needs at least 2 unique characters";
+    errorMessage += "Username needs at least 2 unique characters. ";
   } else if (
     localStorage.get("username") &&
     formData.get("username").toLowerCase() === localStorage.get("username")
   ) {
-    errorMessage = "Username already exists";
+    errorMessage += "Username already exists. ";
   }
   if (formData.get("email") === "") {
-    errorMessage = "Email cannot be empty";
+    errorMessage += "Email cannot be empty. ";
   } else if (!formData.get("email").match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-    errorMessage = "Invalid email format";
+    errorMessage += "Invalid email format. ";
   } else if (formData.get("email").match(/example\.com$/i)) {
-    errorMessage = "Email cannot be from example.com";
+    errorMessage += "Email cannot be from example.com. ";
   }
   if (formData.get("password") === "") {
-    errorMessage = "Password cannot be empty";
+    errorMessage += "Password cannot be empty. ";
   } else if (formData.get("password").length < 12) {
-    errorMessage = "Password must be at least 12 characters long";
+    errorMessage += "Password must be at least 12 characters long. ";
   } else if (!formData.get("password").match(/[a-z]/)) {
-    errorMessage = "Password must contain at least one lowercase letter";
+    errorMessage += "Password must contain at least one lowercase letter. ";
   } else if (!formData.get("password").match(/[A-Z]/)) {
-    errorMessage = "Password must contain at least one uppercase letter";
+    errorMessage += "Password must contain at least one uppercase letter. ";
   } else if (!formData.get("password").match(/\d/)) {
-    errorMessage = "Password must contain at least one number";
+    errorMessage += "Password must contain at least one number. ";
   } else if (!formData.get("password").match(/[^a-zA-Z\d]/)) {
-    errorMessage = "Password must contain at least one special character";
+    errorMessage += "Password must contain at least one special character. ";
   } else if (formData.get("password").match(/password/gi)) {
-    errorMessage = "Password cannot contain the word 'password'";
+    errorMessage += "Password cannot contain the word 'password'. ";
   } else if (formData.get("password") !== formData.get("passwordCheck")) {
-    errorMessage = "Passwords do not match";
+    errorMessage += "Passwords do not match. ";
   }
   if (!formData.get("terms")) {
-    errorMessage = "You must agree to the terms and conditions";
+    errorMessage += "You must agree to the terms and conditions. ";
   }
   if (errorMessage) {
     displayError(errorMessage);
@@ -73,14 +73,14 @@ loginForm.addEventListener("submit", (event) => {
   const localUsername = localStorage.getItem("username");
   const localPassword = localStorage.getItem("password");
   if (formData.get("username") === "") {
-    errorMessage = "Username cannot be empty";
+    errorMessage += "Username cannot be empty. ";
   } else if (formData.get("username").toLowerCase() !== localUsername) {
-    errorMessage = "Username does not exist";
+    errorMessage += "Username does not exist. ";
   }
   if (formData.get("password") === "") {
-    errorMessage = "Password cannot be empty";
+    errorMessage += "Password cannot be empty. ";
   } else if (formData.get("password") !== localPassword) {
-    errorMessage = "Incorrect password";
+    errorMessage += "Incorrect password. ";
   }
   if (errorMessage) {
     displayError(errorMessage);
