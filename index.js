3359da71e5a38bba532617cf3f7677732f032553
Jennifer Hiller <jennhi@gmail.com>
2024-10-01 10:09:32 -0700

    Login form & added localstorage validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const errorDisplay = document.getElementById("errorDisplay");
 const registrationForm = document.getElementById("registration");
+const loginForm = document.getElementById("login");
 function displayError(message) {
   errorDisplay.textContent = message;
   errorDisplay.style.display = "flex";
@@ -22,11 +23,18 @@ registrationForm.addEventListener("submit", (event) => {
     // value needs at least 2 unique characters
   } else if (new Set(formData.get("username")).size < 2) {
     errorMessage = "Username needs at least 2 unique characters";
+  } else if (
+    localStorage.get("username") &&
+    formData.get("username").toLowerCase() === localStorage.get("username")
+  ) {
+    errorMessage = "Username already exists";
   }
   if (formData.get("email") === "") {
     errorMessage = "Email cannot be empty";
   } else if (!formData.get("email").match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
     errorMessage = "Invalid email format";
+  } else if (formData.get("email").match(/example\.com$/i)) {
+    errorMessage = "Email cannot be from example.com";
   }
   if (formData.get("password") === "") {
     errorMessage = "Password cannot be empty";
@@ -52,8 +60,32 @@ registrationForm.addEventListener("submit", (event) => {
     displayError(errorMessage);
   } else {
     hideError();
-    localStorage.setItem("username", formData.get("username"));
+    localStorage.setItem("username", formData.get("username").toLowerCase());
     localStorage.setItem("password", formData.get("password"));
+    localStorage.setItem("email", formData.get("email").toLowerCase());
+    event.target.submit();
+  }
+});
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let errorMessage = "";
+  const formData = new FormData(loginForm);
+  const localUsername = localStorage.getItem("username");
+  const localPassword = localStorage.getItem("password");
+  if (formData.get("username") === "") {
+    errorMessage = "Username cannot be empty";
+  } else if (formData.get("username").toLowerCase() !== localUsername) {
+    errorMessage = "Username does not exist";
+  }
+  if (formData.get("password") === "") {
+    errorMessage = "Password cannot be empty";
+  } else if (formData.get("password") !== localPassword) {
+    errorMessage = "Incorrect password";
+  }
+  if (errorMessage) {
+    displayError(errorMessage);
+  } else {
+    hideError();
     event.target.submit();
   }
 });
